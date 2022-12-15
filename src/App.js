f696b82cacb51417d034842a2f903efc499a2cd2
ammarkhan1904 <ammarkhan1904@gmail.com>
2022-12-15 20:34:11 +0500

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Counter from './Component/Counter';
 import Header from './Component/Header';
 import List from './Component/List';
 
@@ -10,12 +11,14 @@ function App() {
   return (
      <div>
         {/* Passing data as a prop from parent to child */}
-        <Header/>
+        {/* <Header/>
         <List title={titles[0]} img={img[0]} location={location[0]}/>
         <Header/> 
         <List title={titles[1]} img={img[1]} location={location[1]}/>
         <Header/>
-        <List title={titles[2]} img={img[2]} location={location[2]}/>
+        <List title={titles[2]} img={img[2]} location={location[2]}/> */}
+         <Counter/>
+
         </div>
   );
 }
